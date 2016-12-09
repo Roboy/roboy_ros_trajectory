188cf73e0d8f2b551b2af1c4a9a3ed1afa57bb7f
letrend <simon.trendel@tum.de>
2016-12-09 06:45:54 +0100

    with comments

--- src/roboy_ros_trajectory/src/roboy_ros_trajectory.cpp
+++ src/roboy_ros_trajectory/src/roboy_ros_trajectory.cpp
@@ -70,9 +70,11 @@ bool Roboy_Ros_Trajectory::initializeMotors(vector<int8_t> &motors, vector<uint8
     msg.request.controlmode = controlMode;
     msg.request.idList = motors;
     for (auto motor:motors) {
+        // we have to subscribe to the controller status
         char topic[100];
         sprintf(topic, "/roboy/status_motor%d", motor);
         motor_status[motor] = nh->subscribe(topic, 1000, &Roboy_Ros_Trajectory::motorStatus, this);
+        // we set up the trajectory services for every motor
         sprintf(topic, "/roboy/trajectory_motor%d", motor);
         trajectory_srv[motor] = nh->serviceClient<common_utilities::SetTrajectory>(topic);
     }
@@ -83,18 +85,25 @@ int main(int argc, char *argv[]) {
     ros::init(argc, argv, "roboy_ros_trajectory");
 
     Roboy_Ros_Trajectory roboy_ros_trajectory;
+    // the behaviour we want to play is called test and located in ~/.roboy_gui/behaviours
     roboy_ros_trajectory.loadBehaviour("test");
+    // it uses 16 motors
     vector<int8_t> motors = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    // all in position control
     vector<uint8_t> controlmode = {POSITION_CONTROL, POSITION_CONTROL, POSITION_CONTROL, POSITION_CONTROL,
                                    POSITION_CONTROL, POSITION_CONTROL, POSITION_CONTROL, POSITION_CONTROL,
                                    POSITION_CONTROL, POSITION_CONTROL, POSITION_CONTROL, POSITION_CONTROL,
                                    POSITION_CONTROL, POSITION_CONTROL, POSITION_CONTROL, POSITION_CONTROL};
+    // first we need to initialize the ros_control position controllers via a service call
     if (roboy_ros_trajectory.initializeMotors(motors, controlmode)) {
+        // when this was successful, the requested position controllers are running and waiting for trajectories
         int id = roboy_ros_trajectory.behaviours.begin()->first;
+        // we upload the trajectories to the position controllers, who process them
         roboy_ros_trajectory.uploadBehaviour(id);
+        // we send a play steering message
         roboy_ros_trajectory.playBehaviour(id);
     } else {
-        ROS_ERROR("failed to initialize motors, did you launch roboy?\n $roslaunch roboy_hardware roboy.launch");
+        ROS_ERROR("failed to initialize motors, did you launch roboy?\n $ roslaunch roboy_hardware roboy.launch");
     }
 
 
